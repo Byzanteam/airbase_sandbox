fa84ac778189e504f0a80027e3b3e9dd2929bfbb
Phil Chen <06fahchen@gmail.com>
2021-05-26 11:52:27 +0800

    feat: ignore :error result of fallback

--- lib/airbase_sandbox/program_registry/cache.ex
+++ lib/airbase_sandbox/program_registry/cache.ex
@@ -30,8 +30,30 @@ defmodule AirbaseSandbox.ProgramRegistry.Cache do
     )
   end
 
+  @doc """
+  Retrieve the value of a key from cache.
+
+  If the key exits, return the corresponding value, otherwise return the fallback value if provided.
+
+  ## Examples
+
+      iex> AirbaseSandbox.ProgramRegistry.Cache.read(:key)
+      :error
+
+      iex> AirbaseSandbox.ProgramRegistry.Cache.read(:key, fn -> {:ok, :value} end)
+      {:ok, :value}
+
+      iex> Cachex.exists?(AirbaseSandbox.ProgramRegistry.Cache, :key)
+      {:ok, true}
+
+      iex> AirbaseSandbox.ProgramRegistry.Cache.read(:not_exists, fn -> :error end)
+      :error
+
+      iex> Cachex.exists?(AirbaseSandbox.ProgramRegistry.Cache, :not_exists)
+      {:ok, false}
+  """
   @spec read(key :: term(), nil) :: {:ok, term()} | :error
-  @spec read(key :: term(), fallback :: (() -> term())) :: {:ok, term} | :error
+  @spec read(key :: term(), fallback :: (() -> {:ok, term()} | :error)) :: {:ok, term} | :error
   def read(key, fallback \\ nil) do
     case Cachex.get(__MODULE__, key) do
       {:ok, value} when not is_nil(value) ->
@@ -39,10 +61,13 @@ defmodule AirbaseSandbox.ProgramRegistry.Cache do
         {:ok, value}
 
       _ ->
-        if is_function(fallback, 0) do
-          write(key, fallback.())
+        with(
+          true <- is_function(fallback, 0),
+          {:ok, value} <- fallback.()
+        ) do
+          write(key, value)
         else
-          :error
+          _ -> :error
         end
     end
   end
