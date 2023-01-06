08bfbb7922f4ff31b6de50982f66e90a1dde634b
joeywang <joeywdz@gmail.com>
2023-01-06 18:43:42 +0800

    test: add network request related tests

---
+++ test/airbase_sandbox/program/hostcall/networking/response_test.exs
@@ -0,0 +1,24 @@
+defmodule AirbaseSandbox.Program.Hostcall.Networking.ResponseTest do
+  use ExUnit.Case
+  alias AirbaseSandbox.Program.Hostcall.Networking.Response
+
+  test "ok/2 returns a valid response payload" do
+    response = %Finch.Response{
+      body: "{\"name\": \"byzanteam\"}",
+      headers: [
+        {"content-type", "application/json"},
+        {"content-length", "21"},
+        {"accept", "application/json"},
+        {"access-control-allow-origin", "*"},
+        {"via", "1.1 google"},
+        {"alt-svc", "h3=\":443\"; ma=2592000,h3-29=\":443\"; ma=2592000"}
+      ],
+      status: 200
+    }
+
+    assert match?(
+             %AirbaseSandbox.Program.Hostcall.Networking.Response{response: ^response},
+             Response.ok(response)
+           )
+  end
+end
--- test/airbase_sandbox/program/hostcall/networking_test.exs
+++ test/airbase_sandbox/program/hostcall/networking_test.exs
@@ -1,6 +1,6 @@
 defmodule AirbaseSandbox.Program.Hostcall.NetworkingTest do
   use ExUnit.Case
-
+  alias AirbaseSandbox.Program.Hostcall.Networking
   require Logger
 
   setup context do
@@ -15,9 +15,17 @@ defmodule AirbaseSandbox.Program.Hostcall.NetworkingTest do
   end
 
   @tag cd: "test/fixtures"
-  describe "run/2" do
-    test "run the program" do
-      args_binary = "hello"
+  describe "networking run/2" do
+    test "run the networking-sample" do
+      args = [
+        %{
+          "field_type" => "single_line_field",
+          "type" => "literal",
+          "value" => "Asia/Shanghai"
+        }
+      ]
+
+      args_binary = Jason.encode!(args)
 
       assert {:ok, outputs} =
                JetSandbox.Program.run(args_binary,
@@ -26,11 +34,65 @@ defmodule AirbaseSandbox.Program.Hostcall.NetworkingTest do
                  end
                )
 
-      assert outputs === args_binary
+      map = Jason.decode!(outputs) |> List.first()
+      assert {:ok, _datatime} = map["value"] |> Calendar.ISO.parse_naive_datetime()
+    end
+  end
 
-      # kill the instance after run
-      assert %{active: 0, specs: 0, supervisors: 0, workers: 0} ===
-               DynamicSupervisor.count_children(JetSandbox.Program.Server)
+  describe "request/2" do
+    test "returns a response with a successful request" do
+      request_binary =
+        Jason.encode!(%{
+          "method" => "get",
+          "url" => "http://httpbin.org/get"
+        })
+
+      response_binary = Networking.request(request_binary)
+      {:ok, response} = Jason.decode(response_binary)
+      assert response["code"] == 0
+    end
+
+    test "returns a response with invalid json" do
+      request_binary = Jason.encode!(1234)
+      response_binary = Networking.request(request_binary)
+      {:ok, response} = Jason.decode(response_binary)
+      assert response["code"] == 1
+      assert response["message"] == "Expected a map json, got 1234"
+    end
+
+    test "returns a response with a request without method" do
+      request_binary =
+        Jason.encode!(%{
+          "url" => "http://httpbin.org/get"
+        })
+
+      response_binary = Networking.request(request_binary)
+      {:ok, response} = Jason.decode(response_binary)
+      assert response["code"] == 2
+      assert response["message"] == "The method parameter is required."
+    end
+
+    test "returns a response with an invalid method" do
+      request_binary =
+        Jason.encode!(%{
+          "method" => "invalid",
+          "url" => "http://httpbin.org/get"
+        })
+
+      response_binary = Networking.request(request_binary)
+      {:ok, response} = Jason.decode(response_binary)
+      assert response["code"] == 3
+
+      assert response["message"] ==
+               "The method parameter is invalid, expected one of [:get, :post, :put, :patch, :delete, :head, :options], but got \"invalid\"."
+    end
+
+    test "returns a response with an bad_request" do
+      request_binary = "bad_request"
+      response_binary = Networking.request(request_binary)
+      {:ok, response} = Jason.decode(response_binary)
+      assert response["code"] == 4
+      assert response["message"] == "unexpected byte at position 0: 0x62 (\"b\")"
     end
   end
 end
